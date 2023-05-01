4ff34ce74d0bfc5799fa5d280fe40a1b9d842df2
Rostyslav <st1tchqwerty@gmail.com>
2023-05-01 16:48:16 +0200

    Decomposition

--- DinoRunner/Ground.cs
+++ DinoRunner/Ground.cs
@@ -36,51 +36,7 @@ namespace DinoRunner
 
 
 
-    public class Obstacle
-    {
-        private Texture2D _texture;
-        public Vector2 _position;
-
-        public Obstacle(ContentManager content, Vector2 position, int obstacleType)
-        {
-            if (obstacleType == 1)
-            {
-                _texture = content.Load<Texture2D>("OBSTACLE1");
-            }
-            else if (obstacleType == 2)
-            {
-                _texture = content.Load<Texture2D>("OBSTACLE2");
-            }
-            _position = position;
-        }
-
-        public void Update(float speed)
-        {
-            _position.X -= speed;
-        }
-
-        public Rectangle Bounds
-        {
-            get { return new Rectangle((int)_position.X, (int)_position.Y, _texture.Width, _texture.Height); }
-        }
-
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(_texture, _position, Color.White);
-        }
-
-        public Vector2 Position
-        {
-            get { return _position; }
-        }
-
-        public float Width
-        {
-            get { return _texture.Width; }
-        }
-
-    }
+   
 
 }
 
